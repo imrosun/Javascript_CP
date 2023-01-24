16e46ca030370e8139b202ce68922df232dba450
Roshan Sharma <50296636+imrosun@users.noreply.github.com>
2023-01-24 17:02:17 +0530

    solved

--- TreeConstructor.js
+++ TreeConstructor.js
@@ -9,11 +9,8 @@ function TreeConstructor(strArr) {
         if(str[i+1] === ')'){
             parents.push(str[i]);
         }
-    }//should ahve the parnets and kids arrays
-    
-    //kids - 1,2,5,7,9 - 9,1,
-    //pars -2,4,7,2,5 - 4
-    // have to all reduce till i find a single parent
+    }
+  
     let elder = [];
     for (let c of parents){
         if (kids.indexOf(c) === -1){
@@ -30,8 +27,3 @@ function TreeConstructor(strArr) {
    
 // keep this function call here 
 TreeConstructor(readline());                            
-
-// the immediate pattern i see is that there cannot be 3 parents so any istance of two of the same (1,i2)false
-// i have to make sure every kid has a parent and that there is one final parent 
-// mayve i ican make two deparate arrays of the children and parents
-// or make a table of parents and their kids ?
