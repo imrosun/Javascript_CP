ee6e2ad05b8f5bf5a09971a9d8dbb5b5a29a6330
Roshan Sharma <50296636+imrosun@users.noreply.github.com>
2023-01-24 17:01:11 +0530

    Update AgeCounting.php

--- AgeCounting.php
+++ AgeCounting.php
@@ -1,5 +1,4 @@
 <?php
-
   // Your goal is to count how many items exist that have an age equal to or greater than 50, and print this final value.
 
   $ch = curl_init('https://coderbyte.com/api/challenges/json/age-counting');
